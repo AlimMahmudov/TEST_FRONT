8acc0bd9c1d7cbb1baeda53850eade4a2b8f7332
alim <you@example.com>
2024-11-19 13:44:08 +0600

    styles

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect, useCallback, useMemo } from "react";
+import scss from "./HomePage.module.scss";
 
 interface Answer {
   id: number;
@@ -48,7 +49,7 @@ const HomePage: React.FC = () => {
       },
       {
         id: 4,
-        question: "Что означает аббревиатура JS?",
+        question: "Первый Чингизид",
         otvets: [
           { id: 1, text: "Medina", yes: true },
           { id: 2, text: "Alim", yes: false },
@@ -69,9 +70,9 @@ const HomePage: React.FC = () => {
       correctAnswer: string;
     }[]
   >([]);
-  const [finished, setFinished] = useState(false); // Флаг завершения теста
-  const [timeLeft, setTimeLeft] = useState(5); // Таймер для ответа
-  const [testStarted, setTestStarted] = useState(false); // Флаг начала теста
+  const [finished, setFinished] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(5);
+  const [testStarted, setTestStarted] = useState(false);
 
   const nextQuestion = useCallback(() => {
     if (currentQuesti < questions.length - 1) {
@@ -128,73 +129,63 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Тест</h1>
-      {!testStarted ? (
-        <button
-          onClick={() => setTestStarted(true)}
-          style={{
-            padding: "10px 20px",
-            fontSize: "16px",
-            backgroundColor: "#007BFF",
-            color: "#fff",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
-          Начать тест
-        </button>
-      ) : !finished ? (
-        <>
-          <p>{questions[currentQuesti].question}</p>
-          <p>Осталось времени: {timeLeft} сек.</p>
-          <ul>
-            {questions[currentQuesti].otvets.map((answer) => (
-              <li
-                key={answer.id}
-                onClick={() => handleAnswerClick(answer)}
-                style={{
-                  padding: "10px",
-                  margin: "5px 0",
-                  backgroundColor: "#000",
-                  cursor: "pointer",
-                  borderRadius: "4px",
-                }}
-              >
-                {answer.text}
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <div>
-          <h2>Результаты</h2>
-          <ul>
-            {results.map((result, index) => (
-              <li key={index}>
-                <p>
-                  <strong>Вопрос:</strong> {result.question}
-                </p>
-                <p>
-                  <strong>Ваш ответ:</strong>{" "}
-                  <span style={{ color: result.isYes ? "green" : "red" }}>
-                    {result.selectedAnswer}
-                  </span>
-                </p>
-                {!result.isYes && (
-                  <p>
-                    <strong>Правильный ответ:</strong> {result.correctAnswer}
-                  </p>
-                )}
-              </li>
-            ))}
-          </ul>
-          <button onClick={restartTest} style={{ marginTop: "20px" }}>
-            Пройти тест заново
-          </button>
+    <div id={scss.Home}>
+      <div className="container">
+        <div className={scss.home}>
+          <h1>Тест</h1>
+          {!testStarted ? (
+            <button onClick={() => setTestStarted(true)}>Начать тест</button>
+          ) : !finished ? (
+            <>
+              <p>{questions[currentQuesti].question}</p>
+              <p>Осталось времени: {timeLeft} сек.</p>
+              <ul>
+                {questions[currentQuesti].otvets.map((answer) => (
+                  <button
+                    key={answer.id}
+                    onClick={() => handleAnswerClick(answer)}
+                    // style={{
+                    //   padding: "10px",
+                    //   margin: "5px 0",
+                    //   backgroundColor: "#000",
+                    //   cursor: "pointer",
+                    //   borderRadius: "4px",
+                    // }}
+                  >
+                    {answer.text}
+                  </button>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <div className={scss.end}>
+              <h2>Результаты</h2>
+              <ul>
+                {results.map((result, index) => (
+                  <button key={index}>
+                    <p>
+                      <strong>Вопрос:</strong> {result.question}
+                    </p>
+                    <p>
+                      <strong>Ваш ответ:</strong>{" "}
+                      <span style={{ color: result.isYes ? "green" : "red" }}>
+                        {result.selectedAnswer}
+                      </span>
+                    </p>
+                    {!result.isYes && (
+                      <p>
+                        <strong>Правильный ответ:</strong>{" "}
+                        {result.correctAnswer}
+                      </p>
+                    )}
+                  </button>
+                ))}
+              </ul>
+              <button onClick={restartTest}>Пройти тест заново</button>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
